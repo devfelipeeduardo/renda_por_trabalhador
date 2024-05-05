e1fc5a6e35e2b6961ba21810c3c929c001ed67ad
Felipe <felipeeduardofreitasm@gmail.com>
2024-05-05 19:12:24 -0300

    Primeiro commit em exercicio sobre enumerate

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Course.Entities;
+using Course.Entities.Enums;
+
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Order order = new Order{
+
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Delivered
+
+            };
+
+        Console.WriteLine(order);
+
+        string txt = OrderStatus.PendingPayment.ToString();
+
+        OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+        Console.WriteLine(txt);
+        Console.WriteLine(os);
+
+        int statusNumber = (int)order.Status;
+        Console.WriteLine($"N° {statusNumber}");
+
+        }
+
+    }
+
+}
